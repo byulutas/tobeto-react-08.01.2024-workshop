a8ea9cadb00b61b2b30ed094ee4d273387739237
Byulutas <ulutasbayram7@gmail.com>
2024-01-09 23:48:45 +0300

    navbar update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -58,12 +58,6 @@ const Navbar = (props: Props) => {
           )}
           <div className="d-flex align-items-center">
             <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
               <button className="btn btn-outline-success" type="submit">
                 Çıkış
               </button>
